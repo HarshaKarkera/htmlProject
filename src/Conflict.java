84bbc1311fc9e2c2369afb0833551d3c3b462224
HarshaKarkera <116773868+HarshaKarkera@users.noreply.github.com>
2022-11-09 11:49:48 -0500

    Update Conflict.java

--- src/Conflict.java
+++ src/Conflict.java
@@ -3,7 +3,7 @@ public class Conflict
     public static void main(String[] args)
     {
         System.out.println("trying the conflict");
-        System.out.println("second conflict ");
+        System.out.println("second conflict i just made changes ");
 
     }
 }
